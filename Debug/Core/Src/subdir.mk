16da67f26c881b7cccfd2283a114b7377fcd195f
jdmytrva <vdmytruk@gmail.com>
2021-11-23 22:53:15 +0200

    PS-15V05A v1.00

---
+++ Debug/Core/Src/subdir.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Eeprom.c \
+../Core/Src/General.c \
+../Core/Src/Lib.c \
+../Core/Src/Menu.c \
+../Core/Src/MicroMenu2_1.c \
+../Core/Src/hd44780_driver.c \
+../Core/Src/main.c \
+../Core/Src/stm32f1xx_hal_msp.c \
+../Core/Src/stm32f1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f1xx.c \
+../Core/Src/usart.c 
+
+OBJS += \
+./Core/Src/Eeprom.o \
+./Core/Src/General.o \
+./Core/Src/Lib.o \
+./Core/Src/Menu.o \
+./Core/Src/MicroMenu2_1.o \
+./Core/Src/hd44780_driver.o \
+./Core/Src/main.o \
+./Core/Src/stm32f1xx_hal_msp.o \
+./Core/Src/stm32f1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f1xx.o \
+./Core/Src/usart.o 
+
+C_DEPS += \
+./Core/Src/Eeprom.d \
+./Core/Src/General.d \
+./Core/Src/Lib.d \
+./Core/Src/Menu.d \
+./Core/Src/MicroMenu2_1.d \
+./Core/Src/hd44780_driver.d \
+./Core/Src/main.d \
+./Core/Src/stm32f1xx_hal_msp.d \
+./Core/Src/stm32f1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f1xx.d \
+./Core/Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DVDD_VALUE=3300 -DSTM32F100xB -DHSE_VALUE=8000000 -DLSI_VALUE=40000 -DHSI_VALUE=8000000 -DHSE_STARTUP_TIMEOUT=100 -DUSE_FULL_LL_DRIVER -DDEBUG -DLSE_STARTUP_TIMEOUT=5000 -DLSE_VALUE=32768 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
